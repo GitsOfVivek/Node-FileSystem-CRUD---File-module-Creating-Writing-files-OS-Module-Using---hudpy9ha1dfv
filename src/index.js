19311a9fe25b10aa7c6f74212cee60a39d700913
GitsOfVivek <singhvivek1999@yahoo.com>
2023-04-12 12:03:34 +0530

    done

--- src/index.js
+++ src/index.js
@@ -1,26 +1,28 @@
-const fs = require('fs/promises')
+const fs = require('fs/promises');
 
 const myFileWriter = async (fileName, fileContent) => {
 	// write code here
 	// dont chnage function name
-}
+	await fs.writeFile(fileName, fileContent);
+};
 
-const myFileReader = async (fileName) => {
+const myFileReader = async fileName => {
 	// write code here
 	// dont chnage function name
-}
-
+	const result = await fs.readFile(fileName, 'utf-8');
+	return result;
+};
 
 const myFileUpdater = async (fileName, fileContent) => {
 	// write code here
 	// dont chnage function name
-}
+	await fs.appendFile(fileName, fileContent);
+};
 
-const myFileDeleter = async (fileName) => {
+const myFileDeleter = async fileName => {
 	// write code here
 	// dont chnage function name
-}
+	await fs.rm(fileName);
+};
 
-
-
-module.exports = { myFileWriter, myFileUpdater, myFileReader, myFileDeleter }
+module.exports = { myFileWriter, myFileUpdater, myFileReader, myFileDeleter };
